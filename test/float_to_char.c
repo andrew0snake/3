3764b4b07e02655747aa0418694cf9a0b057d5d5
Andrew from rosbt <help@rosbt.com>
2015-03-31 17:40:14 +0600

    123

--- test/float_to_char.c
+++ test/float_to_char.c
@@ -3,10 +3,21 @@
 void main ()
 
 {
-
+    short int i = 0;
     char word = 0;
     float float_word = 0;
+    int two = 0;
+    unsigned int utwo = 0;
     
-    printf ( "word = %c;\nfloat_word = %10f;\n", word, float_word );
+    two = 1;
+    utwo = 1;
+    for ( i = 1; i < 64; ++i ) {
+        two *= 2;
+        utwo *= 2;
+        printf ( "at step i = %12d ( two - 1 )= %12d; ( utwo - 1 ) = %10u;     - ( utwo - 1 ) = %12d \n ", i, two - 1, utwo - 1, - ( utwo - 1 ) );
+        printf ( "float_word = " );
+    
+    };
+    
 
 }
